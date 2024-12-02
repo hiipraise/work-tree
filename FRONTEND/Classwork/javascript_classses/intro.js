ee70a3da09a64b7e362cb4f1a0d6bbada986c91d
tbprayse <obipraisechinedu05@gmail.com>
2024-12-02 14:27:20 +0100

    initial commit

---
+++ FRONTEND/Classwork/javascript_classses/intro.js
@@ -0,0 +1,6 @@
+function number() {
+  for (let i = 100; i >= -100; i--) {
+    console.log(i);
+  }
+}
+number();
---
+++ YouTube_Learnt/javascript/index.js
@@ -0,0 +1,49 @@
+// data type:  undefined, null, boolean, string, symbol, number and object
+
+var myName = "beau";
+myName = 8;
+
+var a;
+var b = "7";
+
+a = 7;
+b = a;
+
+console.log(a);
+
+console.log("Hello world!");
+
+var sum = 10 + 10;
+console.log(sum);
+var difference = sum - 10;
+console.log(difference);
+
+const outPut = "Praise!";
+console.log(outPut);
+
+let result = "Hello world!";
+console.log(result);
+
+var sum = 10 + 40;
+console.log(sum);
+
+var difference = 40 - sum;
+console.log(difference);
+
+var product = 8 * 10;
+console.log(product);
+
+var divide = 10 / 60;
+console.log(divide);
+
+var myVar = 87;
+myVar = myVar + 3;
+myVar--;
+console.log(myVar);
+
+var decimal = 2.5 + 2.5;
+console.log(decimal);
+
+var reminder;
+reminder = 20 % 2;
+console.log(reminder);
