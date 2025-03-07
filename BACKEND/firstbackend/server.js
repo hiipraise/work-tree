dad4e4b35841b433d29f1eb59f78bf49619bfd36
tbprayse <obipraisechinedu05@gmail.com>
2025-03-07 15:49:21 +0100

    backend tutor progress

--- BACKEND/firstbackend/server.js
+++ BACKEND/firstbackend/server.js
@@ -1,6 +1,7 @@
 //import express
 const express = require("express");
 const fs = require("fs");
+const path = require("path");
 
 //instanciate express
 const app = express();
@@ -12,13 +13,13 @@ app.get("/", (req, res) => {
 });
 
 //use the file system to read the content of example.txt
-fs.readFile("example.txt", "utf8", (err, data) => {
-  if (err) {
-    console.error("Error Reading:", err);
-    return;
-  }
-  console.log("Example content is:", data);
-});
+// fs.readFile("example.txt", "utf8", (err, data) => {
+//   if (err) {
+//     console.error("Error Reading:", err);
+//     return;
+//   }
+//   console.log("Example content is:", data);
+// });
 
 app.get("/receipt", (req, res) => {
   fs.readFile("example.txt", "utf8", (err, data) => {
@@ -46,6 +47,70 @@ app.get("/result", (req, res) => {
     }
     res.json({ message: "Student files", data });
   });
+});
+
+// fs.writeFile("newrecord.txt", "This is a new record", (err) => {
+//   if (err) {
+//     console.error("Error Writing:", err);
+//     return;
+//   }
+//   console.log("New record written successfully!");
+// });
+
+//file logger features
+// class Shirt {
+//   constructor(size, color) {
+//     this.size = size;
+//     this.color = color;
+//   }
+// }
+
+// const myShirt = new Shirt("M", "Red");
+// console.log(myShirt);
+
+class FileLogger {
+  constructor(logFilePath, maxSizeMB = 5) {
+    this.logFilePath = logFilePath;
+    this.maxSizeBytes = maxSizeMB * 1024 * 1024;
+  }
+
+  log(message) {
+    const timestamp = new Date().toLocaleString();
+    const logMessage = `[${timestamp}]  ${message}\n`;
+    fs.appendFile(this.logFilePath, logMessage, (err) => {
+      if (err) {
+        console.error("Error writing to log file:", err);
+        return;
+      }
+      console.log("Message logged successfully");
+    });
+  }
+}
+
+const mylogger = new FileLogger(path.join(__dirname, "transrecord.log"));
+const attendanceLogger = new FileLogger(
+  path.join(__dirname, "attendancerecord.log")
+);
+
+//post request to create a new log entry
+app.post("/createlog", (req, res) => {
+  const { amount } = req.body;
+  mylogger.log(`Transaction of ${amount} was successful`);
+  res.send(
+    JSON.stringify({
+      status: "Transaction logged successfully",
+    })
+  );
+});
+
+app.post("/attendance", (req, res) => {
+  const { name } = req.body;
+  attendanceLogger.log(`Attendance of ${name} was successful logged`);
+  res.send(
+    JSON.stringify({
+      status: "Attendance logged successfully",
+    })
+  );
 });
 
 //expose our beackend through api using server to external requests
